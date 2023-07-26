bfbaff58b377820125a569e7d998cc85e6680ecf
yoshi <mdmdmdmdmd57@gmail.com>
2023-07-26 20:36:57 +0900

    front側にrailsから投稿内容をとってきて表示させるところまで実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import { Inter } from 'next/font/google'
+import { Post } from '@/types';
+import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+
+type Props ={
+  posts: Post[];
+};
 
 export async function getStaticProps(){
   const res = await fetch("http://localhost:3001/api/v1/posts/");
@@ -17,8 +24,23 @@ export async function getStaticProps(){
   };
 }
 
-export default function Home() {
+export default function Home({posts}:Props) {
   return (
-    <></>
-      )
+    <>
+    <div>
+      <div>
+        {posts.map((post:Post) =>(
+          <div key={post.id} className={styles.postCard}>
+            <Link href={`posts/${post.id}`} className={styles.postCardBox}>
+            <h2>{post.title}</h2>
+            </Link>
+            <p>{post.content}</p>
+            <button className={styles.editButton}>編集</button>
+            <button className={styles.deleteButton}>削除</button>
+          </div>
+        ))}
+    </div>
+    </div>
+    </>
+  );
 }
